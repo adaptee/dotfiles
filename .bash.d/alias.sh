d397456e0da57e50b8a05b9e84eca9eaa2b1b0d3
Adaptee <adaptee@gmail.com>
2009-09-23 00:05:38 +0800

    refine the output format of ps

--- .bash.d/alias.sh
+++ .bash.d/alias.sh
@@ -136,7 +136,7 @@ alias 755='chmod 755'
 #---------------------------------------------------------------------------#
 
 # customize the displaying layout for command ps.
-alias ps='ps axw -o user,pid,ppid,%cpu,%mem,rss,tname,stat,policy,wchan,start_time,bsdtime,cmd'
+alias ps='ps axw -o user,pid,ppid,%cpu,%mem,rss,tname,stat,policy,wchan,start_time,bsdtime,command'
 
 # '-n' means sort sibling processes by numeric pid, not by name
 alias pstree='pstree -n'
