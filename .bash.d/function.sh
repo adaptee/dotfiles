4bd2d225911bc36e5ba05eaca440fa5967b38548
Adaptee <adaptee@gmail.com>
2010-01-13 20:13:51 +0800

    add function mpf(), which is a convenient wrapper for invoking mplayer with frambbuffer

--- .bash.d/function.sh
+++ .bash.d/function.sh
@@ -208,6 +208,24 @@ function man ()
     LANG=en_US.utf-8 command man $*
 }
 
+
+# simple wrapper for invoking  mplayer nicely with framebuffer
+# mpf stands for mplayer with framebuffer
+function mpf()
+{
+    file="$@"
+
+    # FIXME:
+    #if scale is set to 1024:768(the ideal size), some pixels will remain on the screen side
+    # after mplayer exit.
+    mplayer -vo fbdev2 -fs -x 1024 -y 768 -vf scale=1000:750 -really-quiet "${file}" >& /dev/null &
+    #mplayer -vo fbdev2 -fs -x 1024 -y 768 -vf scale=1024:768 -really-quiet "${file}" >& /dev/null &
+
+    # this is very important ;
+    # clear the screen will make the display not interfered by console output
+    fg ; clear
+}
+
 # grep the result of ps
 # Usage: psg [CMD-NAME]
 # Tip: [\1]  is used to exclude grep from the final result
