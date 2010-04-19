65bba93c90fcef87fcbfcf25622ac19311f1ca49
Adaptee <adaptee@gmail.com>
2010-04-19 10:48:47 +0800

    add the function keyword

--- .bash.d/alias.sh
+++ .bash.d/alias.sh
@@ -107,7 +107,7 @@ alias dus='du -s'
 alias du1='du -h --max-depth=1'
 
 #sort by size; only consider sub-folders.
-du2( )
+function du2 ( )
 {
     du -b --max-depth=1 | sort -nr | perl -pe 's{([0-9]+)}{sprintf "%.1f%s", $1>=2**30? ($1/2**30, "G"): $1>=2**20? ($1/2**20, "M"): $1>=2**10? ($1/2**10, "K"): ($1, "")}e'
 
