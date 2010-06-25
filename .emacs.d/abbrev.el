a00b5eddd4f17b398565aa49090d1119ebd02ab0
Adaptee <adaptee@gmail.com>
2010-06-25 16:55:56 +0800

    add the config files for emacs

---
+++ .emacs.d/abbrev.el
@@ -0,0 +1,19 @@
+;;-*-coding: emacs-mule;-*-
+(define-abbrev-table 'Buffer-menu-mode-abbrev-table '(    ))
+
+(define-abbrev-table 'completion-list-mode-abbrev-table '(    ))
+
+(define-abbrev-table 'fundamental-mode-abbrev-table '(    ))
+
+(define-abbrev-table 'global-abbrev-table '(    ("gg" "google" nil 2)
+    ("st" "saint" nil 2)
+    ))
+
+(define-abbrev-table 'lisp-mode-abbrev-table '(    ))
+
+(define-abbrev-table 'special-mode-abbrev-table '(    ))
+
+(define-abbrev-table 'text-mode-abbrev-table '(    ))
+
+(define-abbrev-table 'vc-git-log-view-mode-abbrev-table '(    ))
+
