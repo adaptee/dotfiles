fdea451b89094289e7a1b8629c43378c0764ae8d
Adaptee <adaptee@gmail.com>
2010-09-20 01:26:35 +0800

    add gentoo specific bash file

---
+++ .bash.d/gentoo.sh
@@ -0,0 +1,145 @@
+#!/bin/bash
+#===============================================================================
+#
+#          FILE:  gentoo.sh
+#
+#         USAGE:  ./gentoo.sh
+#
+#   DESCRIPTION:
+#
+#       OPTIONS:  ---
+#  REQUIREMENTS:  ---
+#          BUGS:  ---
+#         NOTES:  ---
+#        AUTHOR:  adaptee (), adaptee@gmail.com
+#       COMPANY:  Open Source Corporation
+#       VERSION:  1.0
+#       CREATED:  09/16/10 13:18:46 CST
+#      REVISION:  ---
+#===============================================================================
+
+
+#---------------------------------------------------------------------------#
+#                                 package management                        #
+#---------------------------------------------------------------------------#
+
+# installl new package
+# [Example] add firefox
+function add ()
+{
+    sudo emerge "$@"
+}
+
+# remove existing package
+# [Example] purge firefox
+function purge ()
+{
+    sudo emerge --clean "$@"
+    #sudo emerge --unmerge "$@"
+}
+
+function update ()
+{
+    sudo emerge --sync
+}
+
+function upgrade ()
+{
+    sudo emerge -uavDN world
+
+}
+
+function clean ()
+{
+    sudo emerge --depclean --ask
+
+}
+
+# list all installed packages
+function world ()
+{
+    cat /var/lib/portage/world
+}
+
+# add package into 'world
+function explicit()
+{
+    sudo emerge --noreplace "$1"
+}
+
+# list all availabe packages from the repos
+function all ()
+{
+    apt-cache pkgnames
+}
+
+# list what are contained within specified package
+# [Example] list firefox
+function list ()
+{
+    equery files "$1"
+}
+
+# which package own the specified file?
+# [Example] own /usr/bin/vim
+function own ()
+{
+    equery belongs "$1"
+}
+
+# search package by name
+# [Example] search firefox
+function search ()
+{
+    emerge --search "$1"
+    #emerge --searchdesc "$1"
+}
+
+# show the meta info of specified package
+# [Example] meta firefox
+function meta ()
+{
+    equery depends "$1"
+}
+
+# show the USE flags of some package
+# [Example] use firefox
+function use ()
+{
+    equery uses "$1"
+}
+
+# search packages which use specific USE flags
+# [Example] use qt4
+function hasuse ()
+{
+    equery hasuse "$1"
+}
+
+
+
+#---------------------------------------------------------------------------#
+#                                 service management                        #
+#---------------------------------------------------------------------------#
+
+# start service
+# [Example] start smbd
+function start ()
+{
+    sudo /etc/init.d/${1} start
+}
+
+# stop service
+# [Example] stop smbd
+function stop ()
+{
+    sudo /etc/init.d/${1} stop
+}
+
+# restart service
+# [Example] restart smbd
+function restart ()
+{
+    sudo /etc/init.d/${1} restart
+}
+
