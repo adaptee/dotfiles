206687ecacc841955395ec009cf2d1f7263146df
Adaptee <adaptee@gmail.com>
2010-10-11 00:24:26 +0800

    add service starter/stoper convenient function

--- .bash.d/debian.sh
+++ .bash.d/debian.sh
@@ -25,6 +25,31 @@ unset command_not_found_handle
 alias rnet='sudo /etc/init.d/networking restart'
 alias rsmb='sudo /etc/init.d/smbd restart'
 
+#---------------------------------------------------------------------------#
+#                                 service management                        #
+#---------------------------------------------------------------------------#
+
+
+# start service
+# [Example] start smbd
+function start ()
+{
+    sudo /etc/rc.d/${1} start
+}
+
+# stop service
+# [Example] stop smbd
+function stop ()
+{
+    sudo /etc/rc.d/${1} stop
+}
+
+# restart service
+# [Example] restart smbd
+function restart ()
+{
+    sudo /etc/rc.d/${1} restart
+}
 
 #---------------------------------------------------------------------------#
 #                                 package management                        #
