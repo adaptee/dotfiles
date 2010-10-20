2613f46139bac9f054da891aa3133f4723de78fb
Adaptee <adaptee@gmail.com>
2010-10-20 11:23:48 +0800

    upgrade chsdir to version 20101008

--- .bash.d/chs_completion.sh
+++ .bash.d/chs_completion.sh
@@ -1,40 +1,72 @@
-_filedir ()
+_filedir()
 {
-    local IFS='
-' xspec;
-    _expand || return 0;
-    local toks=() tmp;
+    local IFS=$'\t\n' xspec
+
+    _expand || return 0
+
+    local -a toks
+    local tmp
+
+    toks=( ${toks[@]-} $(
+    compgen -d -- "$(quote_readline "$cur")" | {
     while read -r tmp; do
-        [[ -n $tmp ]] && toks[${#toks[@]}]=$tmp;
-    done < <( compgen -d -- "$(quote_readline "$cur")" );
-    if [[ "$1" != -d ]]; then
-        xspec=${1:+"!*.$1"};
-        while read -r tmp; do
-            [[ -n $tmp ]] && toks[${#toks[@]}]=$tmp;
-        done < <( compgen -f -X "$xspec" -- "$(quote_readline "$cur")" );
-    fi;
-    chs=($(chsdir "x$1" "$cur"))
-    COMPREPLY=( "${COMPREPLY[@]}" "${toks[@]}" "${chs[@]}" )
+        echo $tmp
+    done
+}
+))
+
+if [[ "$1" != -d ]]; then
+    xspec=${1:+"!*.$1"}
+    toks=( ${toks[@]-} $(
+    compgen -f -X "$xspec" -- "$(quote_readline "$cur")" | {
+    while read -r tmp; do
+        [ -n $tmp ] && echo $tmp
+    done
+}
+))
+    fi
+	chs=($(chsdir "x$1" "$cur"))
+	COMPREPLY=( "${COMPREPLY[@]}" "${toks[@]}" "${chs[@]}" )
 }
 
-_filedir_xspec ()
+_filedir_xspec()
 {
-    local IFS cur xspec;
-    IFS='
-';
-    COMPREPLY=();
-    cur=`_get_cword`;
-    _expand || return 0;
-    xspec=$( sed -ne '/^complete .*[ 	]'${1##*/}'\([ 	]\|$\)/{p;q;}' 		  $BASH_COMPLETION );
-    xspec=${xspec#*-X };
-    xspec=${xspec%% *};
-    local toks=() tmp;
-    while read -r tmp; do
-        [[ -n $tmp ]] && toks[${#toks[@]}]=$tmp;
-    done < <( compgen -d -- "$(quote_readline "$cur")" );
-    while read -r tmp; do
-        [[ -n $tmp ]] && toks[${#toks[@]}]=$tmp;
-    done < <( eval compgen -f -X $xspec -- "\$(quote_readline "\$cur")" );
+    local IFS cur xspec
+
+    IFS=$'\t\n'
+    COMPREPLY=()
+    cur=`_get_cword`
+
+    _expand || return 0
+
+    xspec=$( sed -ne $'/^complete .*[ \t]'${1##*/}$'\([ \t]\|$\)/{p;q;}' \
+            $BASH_COMPLETION )
+    xspec=${xspec#*-X }
+    xspec=${xspec%% *}
+
+    local -a toks
+    local tmp
+
+    toks=( ${toks[@]-} $(
+        compgen -d -- "$(quote_readline "$cur")" | {
+        while read -r tmp; do
+            # see long TODO comment in _filedir() --David
+            echo $tmp
+        done
+        }
+        ))
+
+    toks=( ${toks[@]-} $(
+        eval compgen -f -X "$xspec" -- "\$(quote_readline "\$cur")" | {
+        while read -r tmp; do
+            [ -n $tmp ] && echo $tmp
+        done
+        }
+        ))
+
     chs=($(chsdir "x$1" "$cur"))
     COMPREPLY=( "${toks[@]}" "${chs[@]}" )
 }
+
+complete -o filenames -F _filedir_xspec file
+
