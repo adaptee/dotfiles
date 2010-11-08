6ea8693dc4499cd1a9b621b33adb668fabce9fb3
Adaptee <adaptee@gmail.com>
2010-11-09 05:48:11 +0800

    update env setting

--- .sh.d/03-env.sh
+++ .sh.d/03-env.sh
@@ -8,8 +8,10 @@ export FCEDIT=$EDITOR
 export PAGER="less"
 export BROWSER="$PAGER"
 
+# --shift : how much to scrool horizontally when pressing <Left>/<Right> arrow
+export LESS='--ignore-case --squeeze-blank-lines --LONG-PROMPT --RAW-CONTROL-CHARS --shift 5 '
+
 # colorful man page
-export LESS='--ignore-case --squeeze-blank-lines --LONG-PROMPT --RAW-CONTROL-CHARS '
 export LESS_TERMCAP_mb=$'\e[01;31m'
 export LESS_TERMCAP_md=$'\e[01;32m'
 export LESS_TERMCAP_me=$'\e[0m'
@@ -17,6 +19,9 @@ export LESS_TERMCAP_se=$'\e[0m'
 export LESS_TERMCAP_so=$'\e[01;44;32m'
 export LESS_TERMCAP_ue=$'\e[01;34m'
 export LESS_TERMCAP_us=$'\e[01;35m'
+
+# default charset/encoding for less
+export LESSCHARSET=utf-8
 
 # Ignore binary files; recursive by default ; highlight found target in results
 export GREP_OPTIONS="--binary-files=without-match --directories=recurse --color=auto"
