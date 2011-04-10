8535043c868008d20ee571e6734cf006853f9675
Adaptee <adaptee@gmail.com>
2011-04-10 20:10:36 +0800

    merge grep related stuff into one file

--- .sh.d/05-grep.sh
+++ .sh.d/05-grep.sh
@@ -5,3 +5,19 @@
 # highlight matching
 export GREP_OPTIONS="--binary-files=without-match --directories=recurse --color=auto"
 export GREP_COLORS="ms=01;31:mc=01;31:sl=:cx=:fn=35:ln=32:bn=32:se=36"
+
+
+#---------------------------------------------------------------------------#
+#                                 Grep quickly                              #
+#---------------------------------------------------------------------------#
+
+alias egrep='egrep -I --color=auto'
+
+# '-I' means returning no match for any binary file
+alias g='grep -I --color=auto'
+
+alias gi='g -i'
+alias gl='g -l'
+alias gn='g -n'
+alias gr='g -r'
+alias gv='g -v'
