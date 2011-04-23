995d7987d5d0084ad954e2952790b420e5dd0847
Jekyll Wu <adaptee@gmail.com>
2011-04-24 00:28:33 +0800

    use gwhich under freebsd

--- .sh.d/51-which.sh
+++ .sh.d/51-which.sh
@@ -3,6 +3,6 @@
 # recommend way of using which, quoted from the manpage
 function which ()
 {
-    (alias; declare -f) | /usr/bin/which --tty-only --read-alias --read-functions --show-tilde --show-dot $@
+    (alias; declare -f) | gwhich --tty-only --read-alias --read-functions --show-tilde --show-dot $@
 }
 #export which
