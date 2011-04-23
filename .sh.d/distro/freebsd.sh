cab4d233ef081f21c562df0f4a4dc424429ab9dc
Jekyll Wu <adaptee@gmail.com>
2011-04-24 00:28:55 +0800

    add primitive functions for freebsd

--- .sh.d/distro/freebsd.sh
+++ .sh.d/distro/freebsd.sh
@@ -1,2 +1,53 @@
 #!/bin/bash
 
+function add ()
+{
+    sudo pkg_add -r "$*"
+}
+
+function purge ()
+{
+    sudo pkg_delete "$*"
+}
+
+
+function list ()
+{
+    pkg_info -L -x "$1"
+}
+
+function own()
+{
+    pkg_info -W "$1"
+}
+
+function meta ()
+{
+    pkg_info -x "$1"
+}
+
+function depends ()
+{
+    pkg_info -r "$1"
+
+}
+
+function rev-depends ()
+{
+    pkg_info -R "$1"
+}
+
+
+function all ()
+{
+    pkg_info
+}
+
+
+
+# quite like iotop under linux
+function io ()
+{
+    top -m io -o total
+}
+
