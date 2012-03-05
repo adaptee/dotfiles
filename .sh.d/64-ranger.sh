73145050e2b2445c01664b11c5b49c1f48c6331f
Jekyll Wu <adaptee@gmail.com>
2012-03-06 06:12:15 +0800

    fix broken option for ranger

--- .sh.d/64-ranger.sh
+++ .sh.d/64-ranger.sh
@@ -3,5 +3,5 @@
 # make ranger utilize the optimization feature provided by ptyhon
 # change $PWD to the last directory , after exiting ranger
 function ranger() {
-    PYTHONOPTIMIZE=1 command ranger --fail-if-run $@ && cd "$(grep \^\' ~/.config/ranger/bookmarks | cut -b3-)"
+    PYTHONOPTIMIZE=1 command ranger $@ && cd "$(grep \^\' ~/.config/ranger/bookmarks | cut -b3-)"
 }
